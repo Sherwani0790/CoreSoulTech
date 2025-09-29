5527c17bf8ea7b7657977f7d78cc6bfd98084c79
Ahmed Sherwani <ahmad.sherwani@appinsnap.com>
2025-09-29 10:56:01 +0500

    NA|Add Team data |01

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import aboutImg from "../../assets/img/communication.svg";
+import team1 from "../../assets/img/team/1.png";
+import team2 from "../../assets/img/team/2.png";
+import team3 from "../../assets/img/team/3.png";
+import team4 from "../../assets/img/team/4.png";
+// import "./About.css"; // custom styles
+
 const About = () => {
+  const team = [
+    {
+      id: 1,
+      name: "Aliza Khan",
+      designation: "Full Stack Developer",
+      img: team1,
+    },
+    {
+      id: 2,
+      name: "Sara Ahmed",
+      designation: "Project Manager",
+      img: team2,
+    },
+    {
+      id: 3,
+      name: "Usman Malik",
+      designation: "Lead Developer",
+      img: team3,
+    },
+    {
+      id: 4,
+      name: "Ayesha Noor",
+      designation: "UI/UX Designer",
+      img: team4,
+    },
+    {
+      id: 5,
+      name: "Zainab Raza",
+      designation: "Documentation Specialist",
+      img: team4,
+    },
+    {
+      id: 6,
+      name: "Hassan Ali",
+      designation: "WordPress Developer",
+      img: team3,
+    },
+  ];
+
   return (
     <>
+      {/* About Section */}
       <section className="py-5">
         <div className="container ">
           <div className="row">
@@ -21,7 +67,7 @@ const About = () => {
             <div className="col-md-6 col-12 p-5">
               <img
                 src={aboutImg}
-                alt=""
+                alt="about"
                 className="img-fluid border-radius-16"
               />
             </div>
@@ -29,13 +75,12 @@ const About = () => {
         </div>
       </section>
 
-      <section className="py-5">
+      {/* Team Section */}
+      <section className="py-5 bg-light">
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center p-5">
-              <h1>Our Team</h1>
-            </div>
-            <div className="col-md-12 text-center">
+              <h1 className="fw-bold">Our Team</h1>
               <p className="lead">
                 Our team is comprised of highly skilled professionals who are
                 passionate about technology and committed to delivering
@@ -45,6 +90,25 @@ const About = () => {
                 clients.
               </p>
             </div>
+          </div>
+
+          {/* Team Cards */}
+          <div className="row justify-content-center">
+            {team.map((member) => (
+              <div key={member.id} className="col-md-3 col-sm-6 mb-4">
+                <div className="team-card">
+                  <img
+                    src={member.img}
+                    alt={member.name}
+                    className="img-fluid"
+                  />
+                  <div className="team-overlay">
+                    <h5>{member.name}</h5>
+                    <p>{member.designation}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
